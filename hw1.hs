660d262592d81ae73d3661f3458b003c95f98b38
Chrisando EP <chrisep8@gmail.com>
2017-02-22 16:42:32 +0800

    Try to solve HW1 from cis194 spring13

---
+++ hw1.hs
@@ -0,0 +1,30 @@
+{-# OPTIONS_GHC -Wall #-}
+
+doubler :: [Integer]->[Integer]
+doubler [] = []
+doubler (x:xs) 
+  | length xs `mod` 2 /= 1 = x : doubler xs
+  | otherwise = x*2 : doubler xs
+
+sum'em :: [Integer]->Integer
+sum'em x = go (doubler x)
+        where 
+         go (y:ys) = combine y + go ys 
+         go [] = 0
+
+combine :: Integer->Integer
+combine x
+  | x < 10 = x
+  | otherwise = x `div` 10 + x `mod` 10
+
+validate::[Integer]->Bool
+validate x = (sum'em x `mod` 10) == 0
+
+
+
+{-# Excerscise 2  - WIP#-}
+
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi amount name1 name2 name3 = [("a", "a")]
