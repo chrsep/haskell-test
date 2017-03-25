7f9934979fcebbeda1fbe390c297fb47bfb7778c
Chrisando EP <chrisep8@gmail.com>
2017-03-25 22:05:58 +0800

    logged several new excercise

---
+++ helloworld.hs
@@ -0,0 +1,9 @@
+import Data.Char
+
+main = do
+  putStrLn "Hello, what's your name?"
+  name <- getLine
+  putStrLn ("Hey " ++ name ++ ", you rock!")
+  let biggerBetter = map toUpper name
+      worse = map toLower $ biggerBetter
+  putStrLn $ "hey," ++ biggerBetter ++ worse
