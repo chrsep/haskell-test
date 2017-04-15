4b29bee95832695d168e3c6e6eb515dc239e066d
Chrisando EP <chrisep8@gmail.com>
2017-04-15 22:19:32 +0800

    added parse & message tree insert

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -5,22 +5,24 @@ module LogAnalysis where
 import Log
 
 parseMessage :: String -> LogMessage
-parseMessage input = go (words input)
-        where
-         go ("I":time:string) =
-          LogMessage Info (read time) (unwords string)
-         go ("E":err:time:string) =
-          LogMessage (Error (read err)) (read time) (unwords string)
-         go ("W":time:string) =
-          LogMessage Warning (read time) (unwords string)
-         go _ = Unknown input
+parseMessage string = case words string of
+  "I":t:m -> LogMessage Info (read t) (unwords m)
+  "W":t:m -> LogMessage Warning (read t) (unwords m)
+  "E":ecode:t:m -> LogMessage (Error $ read ecode) (read t) (unwords m)
+  m ->  Unknown (unwords m)
 
 parse :: String -> [LogMessage]
-parse input = go (lines input)
-        where
-         go (errLog:errLogs) = ((parseMessage errLog):(parse (unlines errLogs)))
-{-
+parse logs = map parseMessage $ lines logs
+
 insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert m@(LogMessage _ t1 _ ) (Node l (LogMessage _ t2 _ ) r)
+  | t1 < t2 = insert m l
+  | t1 > t2 = insert m r
+insert m Leaf = Node Leaf m Leaf
+insert m _ = Node Leaf m Leaf
+
+{-
 
 build :: [LogMessage] -> MessageTree
 
