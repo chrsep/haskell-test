1c02176211ed3798542820c08f701ef1b3fd74c5
Chrisando EP <chrisep8@gmail.com>
2017-04-17 22:18:11 +0800

    Finished 2nd hw

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -2,30 +2,43 @@
 
 module LogAnalysis where
 
-import Log
+import           Log
 
 parseMessage :: String -> LogMessage
 parseMessage string = case words string of
-  "I":t:m -> LogMessage Info (read t) (unwords m)
-  "W":t:m -> LogMessage Warning (read t) (unwords m)
+  "I":t:m       -> LogMessage Info (read t) (unwords m)
+  "W":t:m       -> LogMessage Warning (read t) (unwords m)
   "E":ecode:t:m -> LogMessage (Error $ read ecode) (read t) (unwords m)
-  m ->  Unknown (unwords m)
+  m             ->  Unknown (unwords m)
 
 parse :: String -> [LogMessage]
 parse logs = map parseMessage $ lines logs
 
 insert :: LogMessage -> MessageTree -> MessageTree
 insert (Unknown _) tree = tree
-insert m@(LogMessage _ t1 _ ) (Node l (LogMessage _ t2 _ ) r)
-  | t1 < t2 = insert m l
-  | t1 > t2 = insert m r
+insert m@(LogMessage _ t1 _ ) (Node l mo@(LogMessage _ t2 _ ) r)
+  | t1 < t2 = Node ( insert m l ) mo r
+  | t1 > t2 = Node l mo ( insert m r )
 insert m Leaf = Node Leaf m Leaf
 insert m _ = Node Leaf m Leaf
 
-{-
-
 build :: [LogMessage] -> MessageTree
+build = foldl ( flip insert ) Leaf
 
 inOrder :: MessageTree -> [LogMessage]
+inOrder ( Node l m r) = inOrder l ++ m:[] ++ inOrder r
+inOrder Leaf          = []
 
--}
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = map (\(LogMessage _ _ s) -> s) . filter severe . inOrder . build
+  where
+    severe (LogMessage (Error n)  _ _) = n > 50
+    severe _                           = False
+
+whatWentWrong' :: [LogMessage] -> [String]
+whatWentWrong' = map (\(LogMessage _ _ s) -> s) . filter severe . inOrder . build
+  where
+    severe (LogMessage (Error n)  _ _) = n > 50
+    severe (LogMessage Info t _) = t `elem` [120..131]
+    severe (LogMessage Info t _) = t `elem` [1290..1302]
+    severe _                           = False
