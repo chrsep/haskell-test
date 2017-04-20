2864eedff2b471516fa9e4e1e73f67241423713c
Chrisando EP <chrisep8@gmail.com>
2017-04-20 21:04:20 +0800

    added localMaxima

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -4,3 +4,9 @@ skips :: [a] -> [[a]]
 skips l = map (map snd . filterElem) [1..(length l)]
   where l' = zip [1..] l
         filterElem n = filter (\x -> fst x `mod` n == 0) l'
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima ( x:y:z:zs )
+  | x < y && z < y = y:localMaxima ( y:z:zs )
+  | otherwise = localMaxima ( y:z:zs )
+localMaxima _ = []
